f11d8e49d4ba94d82c771ba1dff8429046dc2490
muhammedOdeh3600 <muhammed.odeh.3600@gmail.com>
2026-08-27 17:14:51 +0300

    Finished making Books Endpoints and making an Author get added automatically if he isn't already in the Authors table when Creating or Updating a Book.

--- BookTracker/Endpoints/BooksEndpoints.cs
+++ BookTracker/Endpoints/BooksEndpoints.cs
@@ -1,3 +1,8 @@
+using BookTracker.Data;
+using BookTracker.Dtos;
+using BookTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace BookTracker.Endpoints;
 
 public static class BooksEndpoints
@@ -7,6 +12,109 @@ public static class BooksEndpoints
     {
         var group = app.MapGroup("/books");
         
+        // GET
+        group.MapGet("/", async (BookTrackerContext dbContext) => 
+            
+            await  dbContext.Books
+                .Select(book => new BookDto(book.Id, book.Title,
+                    book.Author!.Name,
+                    book.Genre!.Name))
+                .AsNoTracking()
+                .ToListAsync()
+            
+            );
+
+        group.MapGet("/{id}", async (int id, BookTrackerContext dbContext) =>
+            {
+                var book = await dbContext.Books.Where(b => b.Id == id)
+                    .Select(book => new BookDto(book.Id,
+                        book.Title,
+                        book.Author!.Name,
+                        book.Genre!.Name)
+                    ).FirstOrDefaultAsync();
+
+                return book is null
+                    ? Results.NotFound() 
+                    : Results.Ok(new BookDto(book.Id, book.Title, book.Author, book.Genre));
+                
+                
+            }
+                    
+            ).WithName(GetBookEndpoint);
         
+        // POST
+
+        group.MapPost("/", async (CreateBookDto createdBookDto, BookTrackerContext dbContext) =>
+
+            {
+                var author = await dbContext.Authors
+                    .FirstOrDefaultAsync(a => a.Name.Trim().ToLower() == createdBookDto.Author.Trim().ToLower());
+                var genre = await dbContext.Genres.FindAsync(createdBookDto.GenreId);
+                if (author is null)
+                {
+                    author = new Author
+                    {
+                        Name = createdBookDto.Author
+                    };
+                }
+
+                Book book = new()
+                {
+                    Title = createdBookDto.Title,
+                    Author = author,
+                    GenreId = createdBookDto.GenreId
+                };
+                
+                dbContext.Books.Add(book);
+                await dbContext.SaveChangesAsync();
+                
+                BookDto bookDto = new BookDto(book.Id, book.Title, createdBookDto.Author, genre!.Name );
+                return Results.CreatedAtRoute(GetBookEndpoint, new { id = book.Id }, bookDto);
+
+            }
+        );
+        
+        // PUT
+
+        group.MapPut("/{id}", async (int id, UpdateBookDto updatedBookDto, BookTrackerContext dbContext)
+                =>
+            {
+                var book =  await dbContext.Books.
+                        FirstOrDefaultAsync(b => b.Id == id);
+                if (book is null)
+                {
+                    return Results.NotFound();
+                }
+                var author = await dbContext.Authors
+                    .FirstOrDefaultAsync(a => a.Name.Trim().ToLower() == updatedBookDto.Author.Trim().ToLower());
+                if (author is null)
+                {
+                    author = new Author
+                    {
+                        Name = updatedBookDto.Author
+                    };
+                }
+                book.Title = updatedBookDto.Title;
+                book.Author = author;
+                book.GenreId = updatedBookDto.GenreId;
+                
+                await dbContext.SaveChangesAsync();
+                
+                return Results.NoContent();
+            }
+            
+            );
+        
+        // DELETE
+
+        group.MapDelete("/{id}", async (int id, BookTrackerContext dbContext)
+                =>
+            {
+                await dbContext.Books.Where(b => b.Id == id).ExecuteDeleteAsync();
+                return Results.NoContent();
+            }
+            
+            );
+
     }
 }
